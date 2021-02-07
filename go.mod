ba6cbbfcd4e40eaf89d0472c2d68467f5dc8c9dc
Daiki Ikehara <serpedious@Daikis-MacBook-Air.local>
2021-02-07 14:57:39 +0900

    introduce chi

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/serpedious/automatic-trading-system
 go 1.16
 
 require (
+	github.com/go-chi/chi v1.5.1 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,48 +3,54 @@ package main
 import (
 	"fmt"
 
+	"net/http"
+
 	"github.com/serpedious/automatic-trading-system/bitflyer"
 	"github.com/serpedious/automatic-trading-system/config"
 	"github.com/serpedious/automatic-trading-system/utils"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/rs/cors"
 )
+
+func public(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello public!\n"))
+}
+
+func private(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello private!\n"))
+}
+
+func balance(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("this is balance form golang!\n"))
+}
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	fmt.Println(apiClient.GetBalance())
+	
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost:*",
+			"https://localhost:*",
+		},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{},
+		AllowCredentials: true,
+		MaxAge:           300,
+		Debug:            true,
+	}).Handler)
+
+	r.Get("/public", public)
+	r.Get("/private", private)
+	r.Get("/balance", balance)
+
+	http.ListenAndServe(":8000", r)
 }
-
-// package main
-
-// import (
-//     "log"
-//     "net/http"
-
-//     "github.com/gorilla/mux"
-// 	"github.com/gorilla/handlers"
-// )
-
-// func public(w http.ResponseWriter, r *http.Request) {
-//     w.Write([]byte("hello public!\n"))
-// }
-
-// func private(w http.ResponseWriter, r *http.Request) {
-//     w.Write([]byte("hello private!\n"))
-// }
-
-// func balance(w http.ResponseWriter, r *http.Request) {
-// 	w.Write([]byte("this is balance form golang!\n"))
-// }
-
-// func main() {
-//     allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8080"})
-//     allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
-//     allowedHeaders := handlers.AllowedHeaders([]string{"Authorization"})
-
-//     r := mux.NewRouter()
-//     r.HandleFunc("/public", public)
-//     r.HandleFunc("/private", private)
-//     r.HandleFunc("/balance", balance)
-
-//     log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r)))
-// }
