5d7f48d4422e4f1cad1aafaee3333eae041c3fe0
serpedious <daiaorab@gmail.com>
2021-02-14 20:27:36 +0900

    change ci conf

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -16,7 +16,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App from ci cd test'
     }
   },
   methods: {
