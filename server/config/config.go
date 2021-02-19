c523fc4ae8d49a3e3322494075cfcd16b49fca6f
serpedious <daiaorab@gmail.com>
2021-02-19 18:43:26 +0900

    adapt production secret

--- server/config/config.go
+++ server/config/config.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 
-	"gopkg.in/ini.v1"
+	"github.com/joho/godotenv"
 )
 
 type ConfigList struct {
@@ -17,16 +17,51 @@ type ConfigList struct {
 var Config ConfigList
 
 func init() {
-	cfg, err := ini.Load("config.ini")
+	var err error
+
+	err = godotenv.Load()
+
 	if err != nil {
 		log.Printf("Failed to read config file: %v", err)
-		os.Exit(1)
 	}
 
 	Config = ConfigList{
-		ApiKey:      cfg.Section("bitflyer").Key("api_key").String(),
-		ApiSecret:   cfg.Section("bitflyer").Key("api_secret").String(),
-		LogFile:     cfg.Section("gotrading").Key("log_file").String(),
-		ProductCode: cfg.Section("gotrading").Key("product_code").String(),
+		ApiKey:      os.Getenv("BITFLYER_API_KEY"),
+		ApiSecret:   os.Getenv("BITFLYER_API_KEY_SECRET"),
+		LogFile:     os.Getenv("LOG_FILE"),
+		ProductCode: os.Getenv("PRODUCT_CODE"),
 	}
 }
+
+// package config
+
+// import (
+// 	"log"
+// 	"os"
+
+// 	"gopkg.in/ini.v1"
+// )
+
+// type ConfigList struct {
+// 	ApiKey      string
+// 	ApiSecret   string
+// 	LogFile     string
+// 	ProductCode string
+// }
+
+// var Config ConfigList
+
+// func init() {
+// 	cfg, err := ini.Load("config.ini")
+// 	if err != nil {
+// 		log.Printf("Failed to read config file: %v", err)
+// 		os.Exit(1)
+// 	}
+
+// 	Config = ConfigList{
+// 		ApiKey:      cfg.Section("bitflyer").Key("api_key").String(),
+// 		ApiSecret:   cfg.Section("bitflyer").Key("api_secret").String(),
+// 		LogFile:     cfg.Section("gotrading").Key("log_file").String(),
+// 		ProductCode: cfg.Section("gotrading").Key("product_code").String(),
+// 	}
+// }
