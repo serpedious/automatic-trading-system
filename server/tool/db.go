f4011c16ed7a58244c9711cd821c2f46cdec975f
serpedious <daiaorab@gmail.com>
2021-02-22 12:42:01 +0900

    setting for rds db

--- server/tool/db.go
+++ server/tool/db.go
@@ -3,6 +3,7 @@ package tool
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/lib/pq"
 	_ "github.com/lib/pq"
@@ -11,15 +12,19 @@ import (
 var Db *sql.DB
 
 func Connect() {
+	var (
+		DATABASE = os.Getenv("POSTGRES_DB")
+		USER     = os.Getenv("POSTGRES_USER")
+		PASSWORD = os.Getenv("PGPASSWORD")
+	)
 	i := Info{}
 	pgUrl, err := pq.ParseURL(i.GetDBUrl())
 	if err != nil {
 		log.Fatal()
 	}
 
-	// Db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=docker dbname=test_db sslmode=disable")
-
-	Db, err = sql.Open("postgres", pgUrl)
+	// neet to implement db connection in the dev env
+	Db, err = sql.Open("postgres", "host="+pgUrl+" port=5432 user="+USER+" password="+PASSWORD+" dbname="+DATABASE+" sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
