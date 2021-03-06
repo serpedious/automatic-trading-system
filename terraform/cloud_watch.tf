75ff8402e9d074bc8f2941aabcdc26820d6cc89f
serpedious <daiaorab@gmail.com>
2021-03-07 00:32:07 +0900

    modify terraform

--- terraform/cloud_watch.tf
+++ terraform/cloud_watch.tf
@@ -2,6 +2,6 @@
 # CloudWatchLog #
 #################
 resource "aws_cloudwatch_log_group" "automatic-trading-system-ecs-api" {
-  name              = "/ecs/api"
+  name              = "/ecs/automatic-trading-system-api"
   retention_in_days = 180
 }
