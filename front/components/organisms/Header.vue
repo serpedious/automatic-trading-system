fd9789f34181adefc11bebce369794801feebebb
serpedious <daiaorab@gmail.com>
2021-03-13 18:24:16 +0900

    create modules header

--- front/components/organisms/Header.vue
+++ front/components/organisms/Header.vue
@@ -1,5 +1,5 @@
 <template>
-<v-app>
+  <div>
     <v-navigation-drawer
       v-model="drawer"
       app
@@ -7,11 +7,11 @@
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
-            Application
+            Dashboard
           </v-list-item-title>
           <v-list-item-subtitle>
-            subtext
-          </v-list-item-subtitle>
+          chose items
+        </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
@@ -38,23 +38,82 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="black"
+      dark
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Automatic Trading system</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-menu
+        bottom
+        left
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            color="blue"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
-  </v-app>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard', to: '/' },
-        { title: 'Memo', icon: 'mdi-image', to: '/auth/signin' },
-        { title: 'About', icon: 'mdi-help-box' },
-      ],
-    }),
-  }
+export default {
+  data: () => ({ 
+    drawer: null,
+    items: [
+      { title: 'Chart', icon: 'mdi-chart-line', to: '/dashboard/chart' },
+      { title: 'Memo', icon: 'mdi-format-list-checks', to: '/dashboard/memo' },
+      { title: 'CSV', icon: 'mdi-file-delimited-outline', to: '/dashboard/csv' },
+      { title: 'Alert', icon: 'mdi-alert-circle', to: '/dashboard/alert' }
+    ],
+  }),
+}
 </script>
+
+<style>
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: rgb(41, 38, 38);
+  letter-spacing: 1px;
+}
+</style>
+
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,94 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Dashboard
-          </v-list-item-title>
-          <v-list-item-subtitle>
-          chose items
-        </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar
-      app
-      color="black"
-      dark
-    >
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
-
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Automatic Trading system</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-menu
-        bottom
-        left
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            color="blue"
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-          >
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </v-app-bar>
+    <Header/>
 
     <v-main>
       <router-view></router-view>
@@ -97,27 +9,14 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-        { title: 'Chart', icon: 'mdi-chart-line', to: '/dashboard/chart' },
-        { title: 'Memo', icon: 'mdi-format-list-checks', to: '/dashboard/memo' },
-        { title: 'CSV', icon: 'mdi-file-delimited-outline', to: '/dashboard/csv' },
-        { title: 'Alert', icon: 'mdi-alert-circle', to: '/dashboard/alert' }
-      ],
-    }),
+import Header from '~/components/organisms/Header.vue'
+export default {
+  components: {
+  Header
   }
+}
 </script>
 
 <style>
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 60px;
-  color: rgb(41, 38, 38);
-  letter-spacing: 1px;
-}
 </style>
 
