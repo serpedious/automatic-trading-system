db0622d8a9ee079aa646b8865adc9f32ae569b9c
serpedious <daiaorab@gmail.com>
2021-11-28 15:03:06 +0900

    add stream ticker tables

--- docker/db/init/1_init.sql
+++ docker/db/init/1_init.sql
@@ -47,8 +47,35 @@ CREATE TABLE tickers (
   updated_at DATE NOT NULL DEFAULT now()
 );
 
+CREATE TABLE signal_events (
+  id serial PRIMARY KEY,
+  time TIMESTAMP NOT NULL,
+  product_code TEXT,
+  side TEXT,
+  price FLOAT,
+  size FLOAT,
+  created_at DATE NOT NULL DEFAULT now(),
+  updated_at DATE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE duration (
+  id serial PRIMARY KEY,
+  time TIMESTAMP NOT NULL,
+  open FLOAT,
+  close FLOAT,
+  high FLOAT,
+  low FLOAT,
+  volume FLOAT,
+  created_at DATE NOT NULL DEFAULT now(),
+  updated_at DATE NOT NULL DEFAULT now()
+);
+
+
+
 INSERT INTO users (email, password) VALUES ('hoge@sample.co.jp', 'golang');
 INSERT INTO memos (content, done, user_id) VALUES ('test', true, 1);
 INSERT INTO profits (day, week, month, total, user_id) VALUES (1, 2, 3, 4, 1);
 INSERT INTO histories (name, side, amount, ta_date, user_id) VALUES ('BTC', 'SELL', 1, '2021-12-31', 1);
 INSERT INTO tickers (day, week, month) VALUES (1, 2, 3);
+INSERT INTO signal_events (time, product_code, side, price, size) VALUES ('2021-12-31', 1, 1, 1.0, 1.0);
+INSERT INTO duration (time, open, close, high, low, volume) VALUES ('2021-12-31', 1.0, 1.0, 1.0, 1.0, 1.0);
