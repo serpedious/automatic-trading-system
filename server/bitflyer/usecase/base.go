850861a7e9312d4c78b454884efbf3490af9a890
serpedious <daiaorab@gmail.com>
2022-01-04 15:08:43 +0900

    infra test

--- server/bitflyer/usecase/base.go
+++ server/bitflyer/usecase/base.go
@@ -3,11 +3,9 @@ package usecase
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
-	"github.com/serpedious/automatic-trading-system/server/config"
 )
 
 const (
@@ -20,32 +18,32 @@ func GetCandleTableName(productCode string, duration time.Duration) string {
 	return fmt.Sprintf("%s_%s", productCode, duration)
 }
 
-func init() {
-	var err error
-	DbConnection, err = sql.Open("sqlite3", config.Config.DbName)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	cmd := fmt.Sprintf(`
-		CREATE TABLE IF NOT EXISTS %s (
-			time DATETIME PRIMARY KEY NOT NULL,
-			product_code STRING,
-			side STRING,
-			price FLOAT,
-			size FLOAT
-		)`, tableNameSignalEvents)
-	DbConnection.Exec(cmd)
+// func init() {
+// 	var err error
+// 	DbConnection, err = sql.Open("sqlite3", config.Config.DbName)
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// 	cmd := fmt.Sprintf(`
+// 		CREATE TABLE IF NOT EXISTS %s (
+// 			time DATETIME PRIMARY KEY NOT NULL,
+// 			product_code STRING,
+// 			side STRING,
+// 			price FLOAT,
+// 			size FLOAT
+// 		)`, tableNameSignalEvents)
+// 	DbConnection.Exec(cmd)
 
-	for _, duration := range config.Config.Durations {
-		tableName := GetCandleTableName(config.Config.ProductCode, duration)
-		c := fmt.Sprintf(`
-		    CREATE TABLE IF NOT EXISTS %s (
-			time DATETIME PRIMARY KEY NOT NULL,
-			open FLOAT,
-			close FLOAT,
-			high FLOAT,
-			low open FLOAT,
-			volume FLOAT)`, tableName)
-		DbConnection.Exec(c)
-	}
-}
+// 	for _, duration := range config.Config.Durations {
+// 		tableName := GetCandleTableName(config.Config.ProductCode, duration)
+// 		c := fmt.Sprintf(`
+// 		    CREATE TABLE IF NOT EXISTS %s (
+// 			time DATETIME PRIMARY KEY NOT NULL,
+// 			open FLOAT,
+// 			close FLOAT,
+// 			high FLOAT,
+// 			low open FLOAT,
+// 			volume FLOAT)`, tableName)
+// 		DbConnection.Exec(c)
+// 	}
+// }
