3af4944bb929860495de2dbd74bc049a8a1c6e69
serpedious <daiaorab@gmail.com>
2022-01-10 00:12:52 +0900

    change db schema

--- docker/db/init/1_init.sql
+++ docker/db/init/1_init.sql
@@ -49,7 +49,7 @@ CREATE TABLE tickers (
 
 CREATE TABLE signal_events (
   id serial PRIMARY KEY,
-  time TIMESTAMP NOT NULL,
+  time TIMESTAMP NOT NULL, 
   product_code TEXT,
   side TEXT,
   price FLOAT,
@@ -58,7 +58,31 @@ CREATE TABLE signal_events (
   updated_at DATE NOT NULL DEFAULT now()
 );
 
-CREATE TABLE duration (
+CREATE TABLE BTC_JPY_1s (
+  id serial PRIMARY KEY,
+  time TIMESTAMP NOT NULL,
+  open FLOAT,
+  close FLOAT,
+  high FLOAT,
+  low FLOAT,
+  volume FLOAT,
+  created_at DATE NOT NULL DEFAULT now(),
+  updated_at DATE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE BTC_JPY_1m0s (
+  id serial PRIMARY KEY,
+  time TIMESTAMP NOT NULL,
+  open FLOAT,
+  close FLOAT,
+  high FLOAT,
+  low FLOAT,
+  volume FLOAT,
+  created_at DATE NOT NULL DEFAULT now(),
+  updated_at DATE NOT NULL DEFAULT now()
+);
+
+CREATE TABLE BTC_JPY_1h0m0s (
   id serial PRIMARY KEY,
   time TIMESTAMP NOT NULL,
   open FLOAT,
@@ -71,11 +95,13 @@ CREATE TABLE duration (
 );
 
 
-
 INSERT INTO users (email, password) VALUES ('guest@gmail.com', 'password');
 INSERT INTO memos (content, done, delete, user_id) VALUES ('test', false, false, 1);
 INSERT INTO profits (day, week, month, total, user_id) VALUES (1, 2, 3, 4, 1);
 INSERT INTO histories (name, side, amount, ta_date, user_id) VALUES ('BTC', 'SELL', 1, '2021-12-31', 1);
 INSERT INTO tickers (day, week, month) VALUES (1, 2, 3);
-INSERT INTO signal_events (time, product_code, side, price, size) VALUES ('2021-12-31', 1, 1, 1.0, 1.0);
-INSERT INTO duration (time, open, close, high, low, volume) VALUES ('2021-12-31', 1.0, 1.0, 1.0, 1.0, 1.0);
+INSERT INTO signal_events (time, product_code, side, price, size) VALUES ('2006-01-02T15:04:05Z07:00', 'BTC_JPY', 'BUY', 555000.03, 1.0);
+INSERT INTO BTC_JPY_1s (time, open, close, high, low, volume) VALUES ('2006-01-02T15:04:05Z07:00', 449.34820443, 449.34820443, 555000.03, 449.34820443, 449.34820443);
+INSERT INTO BTC_JPY_1m0s (time, open, close, high, low, volume) VALUES ('2006-01-02T15:04:05Z07:00', 449.34820443, 449.34820443, 555000.03, 449.34820443, 449.34820443);
+INSERT INTO BTC_JPY_1h0m0s (time, open, close, high, low, volume) VALUES ('2006-01-02T15:04:05Z07:00', 449.34820443, 449.34820443, 555000.03, 449.34820443, 449.34820443);
+
