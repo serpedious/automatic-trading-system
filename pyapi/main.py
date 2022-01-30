1b0c273cca5b98244392ef8dcc30a9d1923a6576
serpedious <daiaorab@gmail.com>
2022-01-30 15:37:34 +0900

    mod connect db for py

--- pyapi/main.py
+++ pyapi/main.py
@@ -1,14 +1,6 @@
-from datetime import datetime
 from threading import Thread
 import sys
 import logging
-
-
-from bitflyer.bitflyer import APIClient
-from app.models.candle import factory_candle_class
-from app.controllers.webserver import ticker
-from app.models.candle import create_candle_with_duration
-
 
 
 
@@ -21,9 +13,6 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 
 if __name__ == "__main__":
-    import app.models
-    from bitflyer.bitflyer import Ticker
-
     from app.models.dfcandle import DataFrameCandle
     df = DataFrameCandle(settings.product_code, settings.trade_duration)
     df.set_all_candles(settings.past_period)
--- pyapi/settings.py
+++ pyapi/settings.py
@@ -1,24 +1,44 @@
-import configparser
+# import configparser
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+# from utils.utils import bool_from_str
+load_dotenv(verbose=True)
 
-from utils.utils import bool_from_str
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 
-conf = configparser.ConfigParser()
-conf.read('settings.ini')
 
-api_key = conf['bitflyer']['api_key']
-api_secret = conf['bitflyer']['api_secret']
-product_code = conf['bitflyer']['product_code']
 
-db_name = conf['db']['name']
-db_driver = conf['db']['driver']
+api_key = os.environ.get('BITFLYER_API_KEY')
+api_secret = os.environ.get('BITFLYER_API_KEY_SECRET')
+product_code = os.environ.get('PRODUCT_CODE')
 
-web_port = int(conf['web']['port'])
 
-trade_duration = conf['pytrading']['trade_duration'].lower()
-back_test = bool_from_str(conf['pytrading']['back_test'])
-past_period = int(conf['pytrading']['past_period'])
-stop_limit_percent = float(conf['pytrading']['stop_limit_percent'])
+web_port = int(os.environ.get('WEB_PORT'))
+
+trade_duration = os.environ.get('TRADE_DURATION')
+past_period = int(os.environ.get('PAST_PERIOD'))
+stop_limit_percent = float(os.environ.get('STOP_LIMIT_PERCENT'))
+
 
 CSV_FILE_PATH='diagnosis.csv'
 TEMPLATE_PATH=''
+
+# conf = configparser.ConfigParser()
+# conf.read('settings.ini')
+
+# api_key = conf['bitflyer']['api_key']
+# api_secret = conf['bitflyer']['api_secret']
+# product_code = conf['bitflyer']['product_code']
+
+# db_name = conf['db']['name']
+# db_driver = conf['db']['driver']
+
+# web_port = int(conf['web']['port'])
+
+# trade_duration = conf['pytrading']['trade_duration'].lower()
+# back_test = bool_from_str(conf['pytrading']['back_test'])
+# past_period = int(conf['pytrading']['past_period'])
+# stop_limit_percent = float(conf['pytrading']['stop_limit_percent'])
